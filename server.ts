600c7cb646ac650104906737607537f33ed05b23
mickceb <cebulski.mi@gmail.com>
2024-04-11 15:17:03 +0200

    adding llm response

--- server.ts
+++ server.ts
@@ -1,10 +1,19 @@
 import express from "express"
+import { getResponseFromLLM } from "./src/llm"
 
 const app = express()
+app.use(express.json())
 const port = process.env.PORT
 
-app.get('/', (req: Request, res: Response) => {
-  res.send("Hello !!!")
+app.post('/', async (req: Request, res: Response, next) => {
+  if(!req.body) return next({ 
+    status: 400,
+    message: "input is required"
+  })
+  
+  const response = await getResponseFromLLM(req.body.input)
+
+  res.json(response)
 })
 
-app.listen(port, () => console.log(`Server is running`))
+await app.listen(port, () => console.log(`Server is running`))
