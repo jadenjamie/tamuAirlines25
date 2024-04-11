aa6bd3748de88e4f5504a6737b0ec7238f6af1f0
mickceb <cebulski.mi@gmail.com>
2024-04-11 14:47:15 +0200

    init prompt template

--- src/promptTemplate.ts
+++ src/promptTemplate.ts
@@ -1,3 +1,8 @@
 
 
-export const PROMPT_TEMPLATE = ``
+export const PROMPT_TEMPLATE = `
+You are an helpful assistant.
+Answer to the questions concisely.
+If you do not know the answer, just say "I would not want to say anything stupid so I would not say anything, but I like you anyway ;)".
+And do not say anything else.
+`
