b608d6567ea684c1ca0f2ec95e8f373243276840
mickceb <cebulski.mi@gmail.com>
2024-04-11 15:15:18 +0200

    init prompt template

--- src/promptTemplate.ts
+++ src/promptTemplate.ts
@@ -5,4 +5,6 @@ You are an helpful assistant.
 Answer to the questions concisely.
 If you do not know the answer, just say "I would not want to say anything stupid so I would not say anything, but I like you anyway ;)".
 And do not say anything else.
+Question: {question}
+Answer:
 `
